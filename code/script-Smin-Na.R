d87b766d0ce5c174681a6df772a4accf113951e1
SminNa <simin.na@berkeley.edu>
2019-10-03 20:04:03 -0700

    code: adding R file

---
+++ code/script-Smin-Na.R
@@ -0,0 +1,15 @@
+# ===========================================================
+# Title: Lab05
+# Description:
+#   This script computes descriptive statistics, as well as
+#   various exploratory data visualizations.
+# Input(s): data file 'nba2018-players.csv'
+# Output(s): summary data files, and plots
+# Auther: Smin Na
+# Data: 10-3-2019
+# ===========================================================
+
+# packages
+library(dplyr)
+library(tidyverse)
+
